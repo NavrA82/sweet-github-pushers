f96dc33691ba626d9dbb4545bf67d7e1f6c68129
ViktoriiaNeroda <viktoriaaaneroda@gmail.com>
2023-04-22 15:32:37 +0300

    2 task

--- index.js
+++ index.js
@@ -4,9 +4,21 @@
 //Якщо користувач ввів "ECMAScript",
 //то показувати через alert: "True!"
 //у противному випадку показати:"Don't you know? ECMAScript!"
-const answer = prompt("кWhat is the official name of JavaScript?");
-if (answer === "ECMAScript") {
-	alert("True!");
-} else {
-	alert("Don't you know? ECMAScript!");
-}
+
+// const answer = prompt("кWhat is the official name of JavaScript?");
+// if (answer === "ECMAScript") {
+// 	alert("True!");
+// } else {
+// 	alert("Don't you know? ECMAScript!");
+// }
+
+
+// 2. Напишіть програму, яка запитає у користувача "Enter the number of minutes to format"
+//число (кількість хвилин) і виведе в консоль
+//рядок у форматі годин і хвилин, тобто 70 має повернути у консоль "01:10"
+
+const answer = prompt("Enter the number of minutes to format");
+const hours = String(Math.floor(answer / 60)).padStart(2, '0') ;
+const minutes = String(answer % 60).padStart(2, '0');
+
+console.log(`${hours} : ${minutes}`);
